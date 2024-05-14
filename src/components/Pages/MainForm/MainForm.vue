c18b0935847c9f44ddb1dc85ff80340dde6481b4
konbraphat51 <konbraphat@gmail.com>
2024-05-14 16:58:23 +0900

    edit template

---
+++ src/components/Pages/MainForm/MainForm.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script>
+export default Vue.defineComponent({
+	name: "App",
+	components: {
+		// LanguageSelection: Vue.defineAsyncComponent(() =>
+		// 	loadModule("src/components/LanguageSelection.vue", options),
+		// ),
+	},
+	setup() {
+		//set up i18n
+		const {t} = VueI18n.useI18n()
+		return {t}
+	},
+})
+</script>
