5be7c75b4508ad60c6a26b62fa98cf8876a2506d
konbraphat51 <konbraphat@gmail.com>
2024-05-14 17:18:21 +0900

    select media

--- src/components/Pages/MainForm/MainForm.vue
+++ src/components/Pages/MainForm/MainForm.vue
@@ -1,5 +1,22 @@
 <template>
-	<div id="MainForm"></div>
+	<div id="MainForm">
+		<h2>
+			{{ t("MainForm.MetaTitle") }}
+		</h2>
+
+		<div class="FormUnit" id="media">
+			<label>
+				{{ t("MainForm.Media") }}
+			</label>
+			<select name="media" id="mediaValue">
+				<option value="netpage">{{ t("MainForm.MediaValue.NetPage") }}</option>
+				<option value="book">{{ t("MainForm.MediaValue.Book") }}</option>
+				<option value="academic">
+					{{ t("MainForm.MediaValue.Academic") }}
+				</option>
+			</select>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -13,3 +30,24 @@ export default Vue.defineComponent({
 	},
 })
 </script>
+
+<i18n>
+    {
+        "en": {
+            "MainForm": {
+                "MetaTitle": "Meta Data",
+                "Media": "Media",
+                "MediaValue": {
+                    "NetPage": "Page on Web",
+                    "Book": "Book",
+                    "Academic": "Academic Resource"
+                }
+            }
+        },
+        "ja": {
+            "MainForm": {
+                "MetaTitle": "メタデータ"
+            }
+        }
+    }
+</i18n>
