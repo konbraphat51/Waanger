83548d4dd8939bd186dbaf21044250f18b687f31
konbraphat51 <konbraphat@gmail.com>
2024-05-22 06:37:15 +0900

    #WriteMetadata

--- src/scripts/MarkDownGenerator.js
+++ src/scripts/MarkDownGenerator.js
@@ -7,5 +7,26 @@ class MarkDownGenerator {
 		this.markDown = ""
 	}
 
-    #
+	/**
+	 * Write metadata
+	 * @param {Object} metadata
+	 * @returns {string}
+	 */
+	#WriteMetadata(metadata) {
+		let metadataString = "<!-- META\n"
+
+		//unix time
+		metadata["CreatedAt"] = new Date().getTime() / 1000.0
+
+		//to JSON
+		metadataJson = JSON.stringify(metadata)
+
+		//write in
+		metadataString += metadataJson
+
+		//ending
+		metadataString += "\nMETA -->\n"
+
+		return metadataString
+	}
 }
