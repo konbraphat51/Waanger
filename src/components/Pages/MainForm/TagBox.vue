baf76bf0acca6c3e099527843fce6b0ecebeb249
konbraphat51 <konbraphat@gmail.com>
2024-05-22 06:47:18 +0900

    TagBox component

---
+++ src/components/Pages/MainForm/TagBox.vue
@@ -0,0 +1,19 @@
+<template>
+	<input type="text" class="TagBox" v-model="tagsRaw" />
+</template>
+
+<script>
+export default Vue.defineComponent({
+	name: "TagBox",
+	data() {
+		return {
+			tagsRaw: "",
+		}
+	},
+	watch: {
+		tagsRaw: function (newVal, oldVal) {
+			this.$emit("OnTagsUpdated", newVal.split(" "))
+		},
+	},
+})
+</script>
