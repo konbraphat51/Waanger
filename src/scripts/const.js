c9569a106355611734c41d144a77babe52a79402
konbraphat51 <konbraphat@gmail.com>
2024-05-22 13:21:31 +0900

    fix typo

--- src/scripts/const.js
+++ src/scripts/const.js
@@ -2,6 +2,6 @@ const locales_writing = ["en", "ja"]
 
 const GITHUB_ACCOUNT = "konbraphat51"
 const GITHUB_REPOSITORY = "ChanwaangReviews"
-const ARITICLE_DIRECTORY = "src/data/articles/each/"
+const ARTICLE_DIRECTORY = "src/data/articles/each/"
 
 const COOKIE_GITHUB_TOKEN = "GTHUB_TOKEN"
