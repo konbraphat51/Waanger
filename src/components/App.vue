24cac429396fd98d108e2eaa4a58280c602ddeb7
konbraphat51 <konbraphat@gmail.com>
2024-05-25 00:16:35 +0900

    add links

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,6 +2,24 @@
 	<div id="app">
 		<!-- view page shown here -->
 		<router-view />
+
+		<a
+			href="https://konbraphat51.github.io/Waanger/"
+			target="_blank"
+			class="WaangerLink"
+		>
+			Konbraphat51 วางer
+		</a>
+
+		<br />
+
+		<a
+			href="https://github.com/konbraphat51/ChanwaangReviews"
+			target="_blank"
+			class="GithubLink"
+		>
+			GitHub Repository
+		</a>
 	</div>
 </template>
 
